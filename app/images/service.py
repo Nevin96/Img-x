34f248b552d9b9a31d88d309ba73d13b3f507688
Nevin Babu <nevinbabu25@gmail.com>
2026-02-21 06:21:56 +0000

    added-image-processing

---
+++ app/images/service.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image as PILImage
+
+VARIANT_DIR = "media/variants"
+
+os.makedirs(VARIANT_DIR,exist_ok=True)
+
+def resize_image(input_path : str,output_path: str,width : int ,height : int):
+    with PILImage.open(input_path) as img:
+        resized = img.resize((width,height))
+        resized.save(output_path)
+
+def create_thumbnail(input_path : str,output_path:str,size = (200,200)):
+    with PILImage.open(input_path) as img:
+        img.thumbnail(size)
+        img.save(output_path)
+
+def conver_format(input_path : str,output_path : str,format : str):
+    with PILImage.open(input_path) as img:
+        rgb = img.convert("RGB")
+        rgb.save(output_path,format=format.upper())
