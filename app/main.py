27f2f4df630b3e7dd6045253e84cc6fa2e4c6351
Nevin Babu <nevinbabu25@gmail.com>
2026-02-21 09:39:27 +0000

    added-image-upload-server

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ from app.core.config import settings
 from app.auth.routes import router as auth_router
 from app.images.routes import router as image_router
 from app.images.transform_routes import router as transform_router
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI(title = 'Imgx - Image Processing API',version='0.1.0')
 app.include_router(auth_router)
 app.include_router(image_router)
 app.include_router(transform_router)
+app.mount("/media",StaticFiles(directory='media'),name='media')
 @app.get("/")
 def status_check():
     return {'status' : 'running',
