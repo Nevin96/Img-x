c3191dbfb482c18bccd831f01e2bba28d85bf43f
Nevin Babu <nevinbabu25@gmail.com>
2026-02-25 06:31:11 +0000

    added-dynamic-routes

---
+++ app/images/dynaminc_routes.py
@@ -0,0 +1,47 @@
+import os 
+from uuid import uuid4
+from fastapi import APIRouter,Depends,HTTPException
+from fastapi.responses import FileResponse
+from sqlalchemy.orm import Session
+
+from app.db.deps import get_db
+from app.auth.deps import get_current_user
+from app.models.image import Image
+from app.models.image_variant import ImageVariant
+from app.images.service import resize_image
+
+router = APIRouter(prefix="/images",tags=['dynamic'])
+@router.get("/{image_id}")
+def dynamic_router(
+    image_id : int,
+    w : int | None = None,
+    h : int | None = None,
+    user_id : str = Depends(get_current_user),
+    db : Session = Depends(get_db)
+):
+    image = db.query(Image).filter(Image.id == image_id).first()
+    if not image:
+        raise HTTPException(404,"Image not found")
+    if not w and not h:
+        return FileResponse(image.file_path)
+    variant = db.query(ImageVariant).filter(ImageVariant.image_id == image_id,
+                                            ImageVariant.width == w,
+                                            ImageVariant.height == h).first()
+    if variant:
+        return FileResponse(variant.file_path)
+    
+    new_name = f"{uuid4()}.jpg"
+    output_path = f"media/variants/{new_name}"
+    resize_image(image.file_path,output_path,w,h)
+
+    variant = ImageVariant(
+        image_id = image_id,
+        file_path = output_path,
+        width = w,
+        height = h,
+        format = 'JPEG',
+        variant_type = 'dynamic'
+    )
+    db.add(variant)
+    db.commit()
+    return FileResponse(output_path)
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.core.config import settings
 from app.auth.routes import router as auth_router
 from app.images.routes import router as image_router
 from app.images.transform_routes import router as transform_router
+from app.images.dynaminc_routes import router as dynamic_router
 from fastapi.staticfiles import StaticFiles
 
 
@@ -10,6 +11,7 @@ app = FastAPI(title = 'Imgx - Image Processing API',version='0.1.0')
 app.include_router(auth_router)
 app.include_router(image_router)
 app.include_router(transform_router)
+app.include_router(dynamic_router)
 app.mount("/media",StaticFiles(directory='media'),name='media')
 @app.get("/")
 def status_check():
