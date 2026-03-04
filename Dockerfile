a3d64a1215ec4eeafa054dcf675a17f30df91340
Nevin Babu <nevinbabu25@gmail.com>
2026-03-04 17:15:38 +0000

    added-docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,21 @@
 FROM python:3.12-slim
+
 WORKDIR /app
-COPY pyproject.toml uv.lock
+
+# Install uv
+RUN pip install uv
+
+# Copy only dependency files
+COPY pyproject.toml uv.lock ./
+
+# Install dependencies into the system site-packages
+# --system tells uv to ignore virtualenvs
+# -r pyproject.toml tells it where the deps are
+RUN uv pip install --system -r pyproject.toml
+
+COPY . .
+
+EXPOSE 8000
+
+# Now uvicorn will be in the system PATH
+CMD ["uv", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,13 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
-    PROJECT_NAME : str
-    SECRET_KEY : str
-    ACCESS_TOKEN_EXPIRE_MINUTES : int
-    REFRESH_TOKEN_EXPIRE_DAYS : int
-    DATABASE_URL : str = "postgresql+psycopg://imgx:imgx@db:5432/imgx"
-
-    class Config:
-        env_file = '.env'
-        case_sensitive = True
+    model_config = ConfigDict(env_file = ".env")
+    PROJECT_NAME: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    DATABASE_URL: str
 
 settings = Settings()
 
