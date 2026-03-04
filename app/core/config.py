ebf28950451de159e9977677ac6a86fad4e30b7a
Nevin Babu <nevinbabu25@gmail.com>
2026-03-04 14:12:49 +0000

    changed-to-postgres

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY : str
     ACCESS_TOKEN_EXPIRE_MINUTES : int
     REFRESH_TOKEN_EXPIRE_DAYS : int
-    DATABASE_URL : str
+    DATABASE_URL : str = "postgresql+psycopg://imgx:imgx@db:5432/imgx"
 
     class Config:
         env_file = '.env'
